03dd13dc2c92a43b860824c0e77bdd4fd111cfb7
GhostXJD <82038257+GhostXJD@users.noreply.github.com>
2024-03-10 21:14:52 -0300

    Conocimientos

--- src/components/Conocimientos.jsx
+++ src/components/Conocimientos.jsx
@@ -16,17 +16,111 @@ import java from '../assets/Conocimientos/java.png';
 
 
 function Conocimientos() {
-  return (
-    <div>
-        <div>
-            <div>
-                <p>Conocimientos</p>
-                <p>Estas son las tecnologias que conozco</p>
+
+    const tecnologias = [
+        {
+            id: 1,
+            src: html,
+            title: 'HTML',
+            style: 'shadow-orange-500'
+        },
+        {
+            id: 2,
+            src: css,
+            title: 'CSS',
+            style: 'shadow-blue-500'
+        },
+        {
+            id: 3,
+            src: javascript,
+            title: 'JavaScript',
+            style: 'shadow-yellow-500'
+        },
+        {
+            id: 4,
+            src: github,
+            title: 'GitHub',
+            style: 'shadow-gray-400'
+        },
+        {
+            id: 5,
+            src: react,
+            title: 'React',
+            style: 'shadow-blue-600'
+        },
+        {
+            id: 6,
+            src: mongo,
+            title: 'Mongo DB',
+            style: 'shadow-yellow-500'
+        },
+        {
+            id: 7,
+            src: node,
+            title: 'Node JS',
+            style: 'shadow-yellow-500'
+        },
+        {
+            id: 8,
+            src: material,
+            title: 'Material UI',
+            style: 'shadow-yellow-500'
+        },
+        {
+            id: 9,
+            src: next,
+            title: 'Next JS',
+            style: 'shadow-white'
+        },
+        {
+            id: 10,
+            src: mysql,
+            title: 'MySQL',
+            style: 'shadow-yellow-500'
+        },
+        {
+            id: 11,
+            src: tailwind,
+            title: 'Tailwind',
+            style: 'shadow-sky-400'
+        },
+        {
+            id: 12,
+            src: python,
+            title: 'Python',
+            style: 'shadow-yellow-500'
+        },
+        {
+            id: 13,
+            src: bootstrap,
+            title: 'Bootstrap',
+            style: 'shadow-yellow-500'
+        },
+        {
+            id: 14,
+            src: java,
+            title: 'Java',
+            style: 'shadow-yellow-500'
+        },
+    ]
+    return (
+        <div name="conocimiento" className="bg-gradient-to-b from-gray-800 to-black w-full h-screen">
+            <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+                <div>
+                    <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Conocimientos</p>
+                    <p className="py-6">Estas son las tecnologias que conozco</p>
+                </div>
+                <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+                    {tecnologias.map(({ id, src, title, style }) => (
+                        <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                            <img src={src} alt="" className="w-20 mx-auto" />
+                            <p className="mt-4">{title}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
-      
-    </div>
-  )
+    )
 }
 
 export default Conocimientos

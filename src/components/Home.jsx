c198f4bef24c9f0b3647e7c7d87f6371aa279d01
GhostXJD <82038257+GhostXJD@users.noreply.github.com>
2024-03-10 23:37:46 -0300

    Scroll de navegacion

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 function Home() {
   return (
-    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+    <div name="Inicio" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
 
       <dix className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row text-white">
         <div className="flex flex-col justify-center h-full">
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from 'react-scroll';
 
 const NavBar = () => {
 
@@ -37,7 +38,7 @@ const NavBar = () => {
                         <li
                             key={id}
                             className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 hover:text-white">
-                            {link}
+                            <Link to={link} smooth duration={500}>{link}</Link>
                         </li>
                     ))}
 
